335d7d256fedd6886c11401dc266fe3c093c15da
[Grant Willis] <[thegwillis.gw@gmail.com]>
2024-02-15 01:22:29 -0700

    Fixed a couple errores in controller and route files

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,8 +1,8 @@
-const express = require("express")
+const express = require('express')
 const router = new express.Router()
 const invController = require('../controllers/invController')
 
 
-router.get("/type/:classificationId", invController.buildByClassificationID);
+router.get("/type/:classificationId", invController.buildByClassificationId);
 
 module.exports = router;
