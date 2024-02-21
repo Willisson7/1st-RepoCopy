b2fdf3be562802147b54a5c4edb068c4b7ef7a5f
[Grant Willis] <[thegwillis.gw@gmail.com]>
2024-02-21 09:50:27 -0700

    Saving work and changed web title

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require('./routes/inventoryRoute')
+const utilities = require('./utilities')
 
 /* ***********************
  * View Engine and Templates.
@@ -31,7 +32,26 @@ app.get('/', baseController.buildHome)
 app.use('/inv', inventoryRoute)
 // app.get('/', function (req, res) {
 //   res.render('index', { title: 'Home' })
-// })
+// File Not Found Route
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, this page is a champion at hide and seek and cannot be found.'})
+})
+
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
